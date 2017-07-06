cecbe8bf6243a8738e23b3e9a9f08afbab8b57a6
Owen Essen <abc_abcwebd@yahoo.com>
2017-07-06 19:36:33 -0400

    Progress on Github authentication. Not perfected yet, but I'm going ahead and commiting because I need to test on Github pages. Github API is returning an error due to environment. Local that is.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,14 @@
 // Initialize Firebase
 // var ref = new Firebase("https://www.gstatic.com/firebasejs/4.1.3/firebase.js");
+
+var config;
+var firebase;
+
 $.getScript('https://www.gstatic.com/firebasejs/4.1.3/firebase.js', function() {
     
 
 
-  var config = {
+  config = {
     apiKey: "AIzaSyDXNX8h3-mZpq6Mv-GslQg_ViYmWJ_zuGM",
     authDomain: "occ-code-feedback-site-db.firebaseapp.com",
     databaseURL: "https://occ-code-feedback-site-db.firebaseio.com",
@@ -13,11 +17,43 @@ $.getScript('https://www.gstatic.com/firebasejs/4.1.3/firebase.js', function() {
     messagingSenderId: "287198859334"
   };
 
-  firebase.initializeApp(config);
 
+
+firebase.initializeApp(config);
+
+  
 
 	// Stuff related to allowing users to sign in with GitHub
 	var provider = new firebase.auth.GithubAuthProvider();
+
+	function githubSignin() {
+		console.log("Sign in function running")
+	   firebase.auth().signInWithPopup(provider)
+	   
+	   .then(function(result) {
+	      var token = result.credential.accessToken;
+	      var user = result.user;
+			
+	      console.log(token)
+	      console.log(user)
+	   }).catch(function(error) {
+	      var errorCode = error.code;
+	      var errorMessage = error.message;
+			
+	      console.log(error.code)
+	      console.log(error.message)
+	   });
+	}
+
+	function githubSignout(){
+	   firebase.auth().signOut()
+	   
+	   .then(function() {
+	      console.log('Signout successful!')
+	   }, function(error) {
+	      console.log('Signout failed')
+	   });
+	}
 
 	// All click events added here
 	$( document ).ready(function() {
@@ -26,8 +62,9 @@ $.getScript('https://www.gstatic.com/firebasejs/4.1.3/firebase.js', function() {
 	  		console.log("Function running")
 	  		var email = $('#email-input').val();
 	  		var password = $('#password-input').val();
+	  		
+	  		// THIS IS FOR SIGNING IN WITH GITHUB
 	  		/*
-	  		THIS IS FOR SIGNING IN WITH GITHUB
 	  		firebase.auth().signInWithPopup(provider).then(function(result) {
 	  			console.log("Inner function running")
 			  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
@@ -46,8 +83,10 @@ $.getScript('https://www.gstatic.com/firebasejs/4.1.3/firebase.js', function() {
 			  // ...
 			});
 			*/
+			
 			console.log(email)
 			console.log(password)
+			/*
 			firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
 		        // Handle Errors here.
 		        var errorCode = error.code;
@@ -61,7 +100,32 @@ $.getScript('https://www.gstatic.com/firebasejs/4.1.3/firebase.js', function() {
 		        console.log(error);
 		        // [END_EXCLUDE]
 		    });
+		    */
+
+		    function toggleSignIn() {
+		      if (!firebase.auth().currentUser) {
+		        // [START createprovider]
+		        var provider = new firebase.auth.GithubAuthProvider();
+		        // [END createprovider]
+		        // [START addscopes]
+		        provider.addScope('repo');
+		        // [END addscopes]
+		        // [START signin]
+		        firebase.auth().signInWithRedirect(provider);
+		        // [END signin]
+		      } else {
+		        // [START signout]
+		        firebase.auth().signOut();
+		        // [END signout]
+		      }
+		    };
 		});
+
+	    $( "#github-signin-btn" ).click(function() {
+	    	console.log("Click event working")
+	    	githubSignin();
+	    });
+
 	});
 
 });
