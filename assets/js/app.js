e2aedb954863d34f9c04ae125ebe06b8d74ecd7a
Owen Essen <abc_abcwebd@yahoo.com>
2017-07-06 20:13:43 -0400

    Integrating GitHub sign in functionality with homepage. Cleaning up code.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,15 @@
 // Initialize Firebase
 // var ref = new Firebase("https://www.gstatic.com/firebasejs/4.1.3/firebase.js");
 
-var config;
-var firebase;
+// var config;
+// var firebase;
 
+// This function gets the firebase js library. All JavaScript that uses that library needs to be inside this function.
 $.getScript('https://www.gstatic.com/firebasejs/4.1.3/firebase.js', function() {
     
 
 
-  config = {
+  var config = {
     apiKey: "AIzaSyDXNX8h3-mZpq6Mv-GslQg_ViYmWJ_zuGM",
     authDomain: "occ-code-feedback-site-db.firebaseapp.com",
     databaseURL: "https://occ-code-feedback-site-db.firebaseio.com",
@@ -57,35 +58,12 @@ firebase.initializeApp(config);
 
 	// All click events added here
 	$( document ).ready(function() {
-	    $( "#sign-in-btn" ).click(function() {
+	    /*$( "#sign-in-btn" ).click(function() {
 	  		event.preventDefault();
 	  		console.log("Function running")
 	  		var email = $('#email-input').val();
-	  		var password = $('#password-input').val();
-	  		
-	  		// THIS IS FOR SIGNING IN WITH GITHUB
-	  		/*
-	  		firebase.auth().signInWithPopup(provider).then(function(result) {
-	  			console.log("Inner function running")
-			  // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-			  var token = result.credential.accessToken;
-			  // The signed-in user info.
-			  var user = result.user;
-			  // ...
-			}).catch(function(error) {
-			  // Handle Errors here.
-			  var errorCode = error.code;
-			  var errorMessage = error.message;
-			  // The email of the user's account used.
-			  var email = error.email;
-			  // The firebase.auth.AuthCredential type that was used.
-			  var credential = error.credential;
-			  // ...
-			});
-			*/
-			
-			console.log(email)
-			console.log(password)
+	  		var password = $('#password-input').val(); */
+
 			/*
 			firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
 		        // Handle Errors here.
@@ -102,7 +80,7 @@ firebase.initializeApp(config);
 		    });
 		    */
 
-		    function toggleSignIn() {
+		/*    function toggleSignIn() {
 		      if (!firebase.auth().currentUser) {
 		        // [START createprovider]
 		        var provider = new firebase.auth.GithubAuthProvider();
@@ -118,10 +96,10 @@ firebase.initializeApp(config);
 		        firebase.auth().signOut();
 		        // [END signout]
 		      }
-		    };
-		});
+		    };*/
+//		});
 
-	    $( "#github-signin-btn" ).click(function() {
+	    $( ".github-signin-btn" ).click(function() {
 	    	console.log("Click event working")
 	    	githubSignin();
 	    });
