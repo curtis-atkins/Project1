34be250281daeeba712f46c4503c08ec8aade142
Owen Essen <abc_abcwebd@yahoo.com>
2017-07-12 16:20:53 -0400

    Troubleshooting comment functionality.

--- assets/js/app.js
+++ assets/js/app.js
@@ -503,6 +503,9 @@ $.getScript('https://www.gstatic.com/firebasejs/4.1.3/firebase.js', function() {
 				var localUpvotes = activeProjectObj.comments[key].upvotes;
 				var localDownvotes = activeProjectObj.comments[key].downvote;
 
+				console.log("localDownvotes: " + localDownvotes);
+				console.log("localUpvotes: " + localDownvotes);
+
 				// App won't display comments that have gotten a large number of downvotes.
 				if (localDownvotes - localUpvotes > 3){
 					var messageHTML = '<ul class="comments-list"><li class="comment"><a class="pull-left" href="#"><img alt="avatar" class="avatar-image" src="' + localPhotoURL + '"></a><div class="comment-body"><div class="comment-heading"><h4 class="user">' + localPoster + '</h4><h5 class="time"></h5></div><p>' + localMessage + '</p></div></li></ul><button class="upvote" data-parent="' + key + '">' + localUpvotes + 'Likes</button><button class="downvote" data-parent="' + key + '">' + localDownvotes + 'Dislikes</button>';
