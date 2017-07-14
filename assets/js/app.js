1984922bd0a8aa36a1261e3a307c13f7c6f68bff
Chris Rackley <misterrackley@gmail.com>
2017-07-14 00:20:02 -0400

    debugging

--- assets/js/app.js
+++ assets/js/app.js
@@ -450,7 +450,7 @@ $.getScript('https://www.gstatic.com/firebasejs/4.1.3/firebase.js', function() {
 		$('#posts-table').empty();
 		$('#posts-table').prepend('<thead><tr><th>Project</th><th>Creator</th><th>Date Posted</th></tr></thead><tbody id="project-list-holder"></tbody>');
 		for (var key in activeRepoPostsObj) {
-			$('#project-list-holder').append('<tr class="success"><td class="project-link"><a>' + activeRepoPostsObj[key].projectName + '</a></td><td><a>' + activeRepoPostsObj[key].owner + '</a></td><td><a>' + activeRepoPostsObj[key].datePosted + '</a></td></tr>');
+			$('#project-list-holder').append('<tr class="success"><td class="project-link">' + activeRepoPostsObj[key].projectName + '</td><td>' + activeRepoPostsObj[key].owner + '</td><td>' + activeRepoPostsObj[key].datePosted + '</td></tr>');
 
 		};
 	}, function(error){
